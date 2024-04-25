b2352b2b59747d5e43da407058406b8c8c79459a
FalcoGer <16250196+FalcoGer@users.noreply.github.com>
2024-04-26 01:24:49 +0200

    simple chat server example

---
+++ examples/chat_server.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../ListeningSocket.h"
+
+// NOLINTNEXTLINE(google-build-using-namespace) // Example code only.
+using namespace CPPSockets;
+
+auto main() -> int
+{
+    const NetAddress         BINDADDR("127.0.0.1");
+    const Port               BINDPORT(4'444);
+
+    std::vector<TCPSocket>   clients {};
+    std::vector<std::string> messageQueue;
+
+    auto                     sock = ListeningSocket(BINDADDR, BINDPORT, false);
+
+    while (true)
+    {
+        auto newClient = sock.accept();
+        if (newClient.has_value())
+        {
+            newClient->setBlocking(false);
+            clients.push_back(std::move(*newClient));
+            std::cout << clients.back() << " connected.\n";
+            newClient->send("Welcome to the chat.\n");
+            for (auto& client : clients)
+            {
+                messageQueue.push_back(
+                  std::format("{}:{} has joined.\n", client.getAddress().data(), client.getPort().data())
+                );
+            }
+        }
+        for (auto& client : clients)
+        {
+            if (client.queryConnectionClosed())
+            {
+                std::cout << client << " disconnected.\n";
+                messageQueue.push_back(
+                  std::format("{}:{} has left.\n", client.getAddress().data(), client.getPort().data())
+                );
+                continue;
+            }
+            auto msg = client.recv();
+            if (msg.has_value())
+            {
+                std::cout << client << ": " << *msg << '\n';
+                messageQueue.push_back(
+                  std::format("[ {}:{} ]: {}\n", client.getAddress().data(), client.getPort().data(), *msg)
+                );
+            }
+        }
+
+        std::erase_if(clients, [](TCPSocket& client) { return !client.isOpen(); });
+
+        for (auto& client : clients)
+        {
+            for (auto& msg : messageQueue)
+            {
+                client.send(msg);
+                if (!client.isOpen())
+                {
+                    break;
+                }
+            }
+        }
+
+        messageQueue.clear();
+    }
+}
